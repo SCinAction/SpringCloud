4cc98878294b9968fa8d09e9d9ff57c8a25a18a8
custa <custa@126.com>
2018-11-09 17:36:48 +0800

    hello 增加 Swagger 页面

--- hello/src/main/java/com/example/hello/HelloController.java
+++ hello/src/main/java/com/example/hello/HelloController.java
@@ -1,8 +1,11 @@
 package com.example.hello;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +14,15 @@ import java.util.Random;
 
 @Slf4j
 @RestController
+@RequestMapping("/hello")
+@Api(value = "Hello", tags = "Hello 资源")
 public class HelloController {
 
 	@Value("${greeting}")
 	private String greeting;
 
-	@RequestMapping("/hello")
+	@GetMapping("")
+	@ApiOperation(value = "查询 Hello")
 	public String hello(@RequestHeader HttpHeaders headers) throws InterruptedException {
 		log.info("headers: " + headers.toString());
 		int sleepTime = new Random().nextInt(2000);
